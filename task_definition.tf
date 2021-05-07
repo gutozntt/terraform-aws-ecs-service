2304d7a9392b7a3a4aadbb5a9ff09a03c2b00894
Gustavo Zanotto <gzanotto@missioncloud.com>
2021-05-07 09:18:13 -0300

    Adding firelens to the task def

--- task_definition.tf
+++ task_definition.tf
@@ -1,7 +1,7 @@
 resource "aws_ecs_task_definition" "this" {
   count                 = var.task_definition == null ? 1 : 0
   family                = "${var.environment}-${var.name}-task-definition"
-  container_definitions = "[${local.container_json_map}]"
+  container_definitions = var.enable_firelens ? "[${local.container_json_map},${local.firelens_container_json_map}]" : "[${local.container_json_map}]"
   execution_role_arn    = var.execution_role_arn
   tags                  = merge({ "Name" = "${var.environment}-${var.name}-task-definition" }, var.tags)
 }
